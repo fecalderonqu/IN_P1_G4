9c790cdbaa4701d97e3fb5571fb2ce457d0a7809
fercalderon <fernando.calderon@tikee.tech>
2025-09-08 22:39:28 -0500

    listo para crear dfs

--- data_to_DB.py
+++ data_to_DB.py
@@ -1,6 +1,5 @@
 import psycopg2
 import pandas as pd
-from concurrent.futures import ThreadPoolExecutor
 
 DB_CONFIG = {
     'host': 'localhost',
@@ -10,7 +9,7 @@ DB_CONFIG = {
     'dbname': 'postgres'
 }
 DB_ANOMALIAS = 'postgres'
-CSV_PATH = '/Users/fernandocalderon05/Escritorio2/UIDE/MAESTRIA/4. INTELIGENCIA DE NEGOCIOS/PRACTICA 1/Practica1_G4/IN_P1_G4/dataset/population_data.csv'
+CSV_PATH = ['/Users/fernandocalderon05/Escritorio2/UIDE/MAESTRIA/4. INTELIGENCIA DE NEGOCIOS/PRACTICA 1/Practica1_G4/IN_P1_G4/dataset/population_data.csv']
 BATCH_SIZE = 10
 MAX_WORKERS = 10
 
@@ -25,14 +24,47 @@ def verificar_conexion():
     except Exception as e:
         print(f"❌ Error al conectar: {e}")
 
-def insertar_filas_en_bloque(df):
+def crear_tabla(column_names, tabla='population'):
+    columns_str = ', '.join([f'"{col}" VARCHAR(100)' for col in column_names])
+    sql = f"CREATE TABLE IF NOT EXISTS {tabla} (id SERIAL PRIMARY KEY,{columns_str});"
     try:
-        for i in range(0, len(df), BATCH_SIZE):
-            bloque = df.iloc[i:i + BATCH_SIZE][['id_usario', 'cluster']]
-            with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-                executor.map(insertar_registro, [row for _, row in bloque.iterrows()])
+        with psycopg2.connect(**DB_CONFIG) as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql)
+                conn.commit()
+        print("✅ Tabla creada.")
     except Exception as e:
-        print(f"❌ Error al insertar registros por bloques: {e}")
+        print(f"❌ Error al crear la tabla: {e}")
+
+
+def consulta_a_dataframe(query, params=None):
+    try:
+        with psycopg2.connect(**DB_CONFIG) as conn:
+            df = pd.read_sql_query(query, conn, params=params)
+        print("✅ Consulta ejecutada con éxito.")
+        return df
+    except Exception as e:
+        print(f"❌ Error al ejecutar consulta: {e}")
+        return pd.DataFrame()  # retorna vacío si falla
+
+
+def insertar_registros(data, tabla='population'):
+    try:
+        # Columnas entre comillas dobles si tienen espacios o mayúsculas
+        columnas = ', '.join([f'"{col}"' for col in data.columns])
+        placeholders = ', '.join(['%s'] * len(data.columns))
+        sql = f"INSERT INTO {tabla} ({columnas}) VALUES ({placeholders});"
+        with psycopg2.connect(**DB_CONFIG) as conn:
+            with conn.cursor() as cur:
+                for row in data.itertuples(index=False, name=None):
+                    cur.execute(sql, row)
+            conn.commit()
+
+        print(f"✅ Insertados {len(data)} registros.")
+
+    except Exception as e:
+        print(f"❌ Error al insertar dato: {e}")
+
 
 
 def main():
@@ -40,15 +72,20 @@ def main():
         verificar_conexion()
 
         print("⏳ Leyendo archivo CSV...")
-        df = pd.read_csv(CSV_PATH, delimiter=',', encoding='utf-8')
+        for csv_path in CSV_PATH:
+            df = pd.read_csv(csv_path, delimiter=',', encoding='utf-8')
+            column_names = df.columns.tolist()
+            crear_tabla(column_names)
+            insertar_registros(df)
+
+        ## pueden crear varios df en base a la consulta que deseen hacer por ejemplo: df1
+        df1 = consulta_a_dataframe("SELECT * FROM population LIMIT 5;")
+        df1.drop(columns=['Unnamed: 62'], inplace=True)
+
 
         print("⏳ Insertando registros en la base de datos...")
-        insertar_filas_en_bloque(df)
-
-        print("✅ Inserción con batch completada.")
     except Exception as e:
         print(f"❌ Error en el proceso principal: {e}")
-
 
 if __name__ == '__main__':
     main()
